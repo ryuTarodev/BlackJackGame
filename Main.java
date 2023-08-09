7d291f4a1b60190e1c4a362a3e986379e87f213d
ryuTarodev <tarozaza3@gmail.com>
2023-08-09 10:38:34 +0700

    add playAgain choice

--- Main.java
+++ Main.java
@@ -11,31 +11,40 @@ public class Main {
         System.out.println("Welcome to BlackJack!");
         deck.shuffle();
         game.deal();
-
         while (true) {
-            System.out.println("Player hand: " + game.getPlayerScore());
-            System.out.println("PlayerCard:");
-            // String formmattedCard = "";
-            for (int i = 0; i < playerHand.size(); i++) {
-                // formmattedCard += playerHand.get(i).toString();
-                System.out.println(playerHand.get(i).toString());
+            while (true) {
+                System.out.println("Player hand: " + game.getPlayerScore());
+                System.out.println("PlayerCard:");
+                // String formmattedCard = "";
+                for (int i = 0; i < playerHand.size(); i++) {
+                    // formmattedCard += playerHand.get(i).toString();
+                    System.out.println(playerHand.get(i).toString());
+                }
+                // System.out.println(formmattedCard);
+                System.out.println("Would you like to Hit or Stay?");
+                System.out.println("1 : Hit");
+                System.out.println("2 : Stay");
+                System.out.print("Choice: ");
+                Scanner sc = new Scanner(System.in);
+                int choice = sc.nextInt();
+                if (choice == 1) {
+                    game.hitPlayer();
+                    game.hitDealer();
+                } else if (choice == 2) {
+                    System.out.println("Player hand: " + game.getPlayerScore());
+                    System.out.println("Dealer hand: " + game.getDealerScore());
+                    game.determineWinner();
+                    break;
+                }
             }
-            // System.out.println(formmattedCard);
-            System.out.println("Would you like to Hit or Stay?");
-            System.out.println("1 : Hit");
-            System.out.println("2 : Stay");
+            System.out.println("Would you like to play again? Type 1 for yes, 2 for no.");
             System.out.print("Choice: ");
             Scanner sc = new Scanner(System.in);
             int choice = sc.nextInt();
-            if (choice == 1) {
-                game.hitPlayer();
-                game.hitDealer();
-            } else if (choice == 2) {
-                System.out.println("Player hand: " + game.getPlayerScore());
-                System.out.println("Dealer hand: " + game.getDealerScore());
-                game.determineWinner();
+            if (choice == 2) {
                 break;
             }
+
         }
 
     }
